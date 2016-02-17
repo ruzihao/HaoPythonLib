bb5ed8f80c5f4d2a2efed15cb80e154dd292f83b
Hao Zhong <zhhao31@gmail.com>
2016-02-17 09:08:00 -0500

    init commit

---
+++ CF-toolkit/eachMovieComparisonScript.m
@@ -0,0 +1,22 @@
+S.numUsersLimit=5000;
+S.activePerc=0.5;
+S.percReported=0.5;
+S.percPredicted=0.5;
+S.crossValNum=1;
+S.K=10;
+S.coeff=0.5;
+S.sigma=0.7;
+S.numValues=6;
+
+[sim1Err,sim2Err,PDErr,avgErr,constErr,S]=eachMovieComparison(S);
+save experiment1 sim1Err sim2Err PDErr avgErr constErr S;
+
+tp=1;
+disp([mean(sim1Err{1}(tp,:)) mean(sim2Err{1}(tp,:)) ...
+        mean(PDErr{1}(tp,:)) mean(avgErr{1}(tp,:)) mean(constErr{1}(tp,:)) ]);
+tp=2;
+disp([mean(sim1Err{1}(tp,:)) mean(sim2Err{1}(tp,:)) ...
+        mean(PDErr{1}(tp,:)) mean(avgErr{1}(tp,:)) mean(constErr{1}(tp,:)) ]);
+tp=3;
+disp([mean(sim1Err{1}(tp,:)) mean(sim2Err{1}(tp,:)) ...
+        mean(PDErr{1}(tp,:)) mean(avgErr{1}(tp,:)) mean(constErr{1}(tp,:)) ]);
---
+++ startup.py
@@ -0,0 +1,7 @@
+import sys
+
+path_to_add = [r'f:\Dropbox\Documents\Programming\Python\My_Python_Lib',
+               r'F:\Dropbox\Documents\Rutgers\Internship\BellLabs\Stuffs',
+               r'F:\Dropbox\Documents\Projects\QuantIn']
+for x in path_to_add:
+	sys.path.append(x)
