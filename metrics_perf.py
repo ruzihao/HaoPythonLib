e4d7fabb38654ddb9b46e07148b0178a6e0d4bb5
Howard Zhong <zhhao31@gmail.com>
2020-07-13 16:57:35 +0800

    add: metrics_perf

---
+++ metrics_perf.py
@@ -0,0 +1,334 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import precision_recall_curve, average_precision_score
+from matplotlib import use
+use("Agg")
+import matplotlib.pyplot as plt
+import csv,argparse
+import warnings
+warnings.filterwarnings("ignore")
+# For plotting inline in notebook
+# %matplotlib inline 
+
+
+def get_roc(df,score,target,title='',plot=1):
+    df1 = df[[score,target]].dropna()
+    fpr, tpr, thresholds = roc_curve(df1[target], df1[score])
+    ks=np.abs(tpr-fpr)
+    if plot==1:
+    # Plot ROC curve
+        plt.figure(figsize=(6,4))
+        plt.plot(fpr, tpr, label='AUC=%0.2f KS=%0.2f' %(auc(fpr, tpr),ks.max()))
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.grid(b=True, which='both', color='0.65',linestyle='-')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(title+'Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
+    return auc(fpr, tpr),np.max(np.abs(tpr-fpr)),thresholds[ks.argmax()]
+
+
+def get_cum_gains(df,score,target,title):
+    df1 = df[[score,target]].dropna()
+    fpr, tpr, thresholds = roc_curve(df1[target], df1[score])
+    ppr=(tpr*df[target].sum()+fpr*(df[target].count()-df[target].sum()))/df[target].count()
+    plt.figure(figsize=(12,4))
+    plt.subplot(1,2,1)
+    plt.plot(ppr, tpr, label='')
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.grid(b=True, which='both', color='0.65',linestyle='-')
+    plt.xlabel('%Population')
+    plt.ylabel('%Target')
+    plt.title(title+'Cumulative Gains Chart')
+    plt.legend(loc="lower right")
+
+    plt.subplot(1,2,2)
+    plt.plot(ppr, tpr/ppr, label='')
+    plt.plot([0, 1], [1, 1], 'k--')
+    plt.grid(b=True, which='both', color='0.65',linestyle='-')
+    plt.xlabel('%Population')
+    plt.ylabel('Lift')
+    plt.title(title+'Lift Curve')
+
+
+def get_precision_recall(df,score,target,title):
+    precision, recall, _ = precision_recall_curve(df[target], df[score])
+    roc_pr = average_precision_score(df[target], df[score])
+    # Plot ROC curve
+    plt.figure(figsize=(6,4))
+    plt.plot(recall, precision, label='Precision-Recall curve (AUC = %0.2f)' % roc_pr)
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title(title+"Precision-Recall Curve")
+    plt.legend(loc="lower left")
+    plt.grid(b=True, which='both', color='0.65',linestyle='-')
+
+
+def get_deciles_analysis(df,score,target):
+    df1 = df[[score,target]].dropna()
+    _,bins = pd.qcut(df1[score],10,retbins=True)
+    bins[0] -= 0.001
+    bins[-1] += 0.001
+    bins_labels = ['%d.(%0.2f,%0.2f]'%(9-x[0],x[1][0],x[1][1]) for x in enumerate(zip(bins[:-1],bins[1:]))]
+    bins_labels[0] = bins_labels[0].replace('(','[')
+    df1['Decile']=pd.cut(df1[score],bins=bins,labels=bins_labels)
+    df1['Population']=1
+    df1['Zeros']=1-df1[target]
+    df1['Ones']=df1[target]
+    summary=df1.groupby(['Decile'])[['Ones','Zeros','Population']].sum()
+    summary=summary.sort_index(ascending=False)
+    summary['TargetRate']=summary['Ones']/summary['Population']
+    summary['CumulativeTargetRate']=summary['Ones'].cumsum()/summary['Population'].cumsum()
+    summary['TargetsCaptured']=summary['Ones'].cumsum()/summary['Ones'].sum()
+    return summary
+
+
+def confusion_matrix(rater_a, rater_b, min_rating=None, max_rating=None):
+    """
+    Returns the confusion matrix between rater's ratings
+    """
+    assert(len(rater_a) == len(rater_b))
+    if min_rating is None:
+        min_rating = min(rater_a + rater_b)
+    if max_rating is None:
+        max_rating = max(rater_a + rater_b)
+    num_ratings = int(max_rating - min_rating + 1)
+    conf_mat = [[0 for i in range(num_ratings)]
+                for j in range(num_ratings)]
+    for a, b in zip(rater_a, rater_b):
+        conf_mat[a - min_rating][b - min_rating] += 1
+    return conf_mat
+
+
+def histogram(ratings, min_rating=None, max_rating=None):
+    """
+    Returns the counts of each type of rating that a rater made
+    """
+    if min_rating is None:
+        min_rating = min(ratings)
+    if max_rating is None:
+        max_rating = max(ratings)
+    num_ratings = int(max_rating - min_rating + 1)
+    hist_ratings = [0 for x in range(num_ratings)]
+    for r in ratings:
+        hist_ratings[r - min_rating] += 1
+    return hist_ratings
+
+
+def quadratic_weighted_kappa(rater_a, rater_b, min_rating=None, max_rating=None):
+    """
+    Calculates the quadratic weighted kappa
+    quadratic_weighted_kappa calculates the quadratic weighted kappa
+    value, which is a measure of inter-rater agreement between two raters
+    that provide discrete numeric ratings.  Potential values range from -1
+    (representing complete disagreement) to 1 (representing complete
+    agreement).  A kappa value of 0 is expected if all agreement is due to
+    chance.
+    quadratic_weighted_kappa(rater_a, rater_b), where rater_a and rater_b
+    each correspond to a list of integer ratings.  These lists must have the
+    same length.
+    The ratings should be integers, and it is assumed that they contain
+    the complete range of possible ratings.
+    quadratic_weighted_kappa(X, min_rating, max_rating), where min_rating
+    is the minimum possible rating, and max_rating is the maximum possible
+    rating
+    """
+    rater_a = np.array(rater_a, dtype=int)
+    rater_b = np.array(rater_b, dtype=int)
+    assert(len(rater_a) == len(rater_b))
+    if min_rating is None:
+        min_rating = min(min(rater_a), min(rater_b))
+    if max_rating is None:
+        max_rating = max(max(rater_a), max(rater_b))
+    conf_mat = confusion_matrix(rater_a, rater_b,
+                                min_rating, max_rating)
+    num_ratings = len(conf_mat)
+    num_scored_items = float(len(rater_a))
+
+    hist_rater_a = histogram(rater_a, min_rating, max_rating)
+    hist_rater_b = histogram(rater_b, min_rating, max_rating)
+
+    numerator = 0.0
+    denominator = 0.0
+
+    for i in range(num_ratings):
+        for j in range(num_ratings):
+            expected_count = (hist_rater_a[i] * hist_rater_b[j]
+                              / num_scored_items)
+            d = pow(i - j, 2.0) / pow(num_ratings - 1, 2.0)
+            numerator += d * conf_mat[i][j] / num_scored_items
+            denominator += d * expected_count / num_scored_items
+
+    return 1.0 - numerator / denominator
+
+
+def linear_weighted_kappa(rater_a, rater_b, min_rating=None, max_rating=None):
+    """
+    Calculates the linear weighted kappa
+    linear_weighted_kappa calculates the linear weighted kappa
+    value, which is a measure of inter-rater agreement between two raters
+    that provide discrete numeric ratings.  Potential values range from -1
+    (representing complete disagreement) to 1 (representing complete
+    agreement).  A kappa value of 0 is expected if all agreement is due to
+    chance.
+    linear_weighted_kappa(rater_a, rater_b), where rater_a and rater_b
+    each correspond to a list of integer ratings.  These lists must have the
+    same length.
+    The ratings should be integers, and it is assumed that they contain
+    the complete range of possible ratings.
+    linear_weighted_kappa(X, min_rating, max_rating), where min_rating
+    is the minimum possible rating, and max_rating is the maximum possible
+    rating
+    """
+    assert(len(rater_a) == len(rater_b))
+    if min_rating is None:
+        min_rating = min(rater_a + rater_b)
+    if max_rating is None:
+        max_rating = max(rater_a + rater_b)
+    conf_mat = confusion_matrix(rater_a, rater_b,
+                                min_rating, max_rating)
+    num_ratings = len(conf_mat)
+    num_scored_items = float(len(rater_a))
+
+    hist_rater_a = histogram(rater_a, min_rating, max_rating)
+    hist_rater_b = histogram(rater_b, min_rating, max_rating)
+
+    numerator = 0.0
+    denominator = 0.0
+
+    for i in range(num_ratings):
+        for j in range(num_ratings):
+            expected_count = (hist_rater_a[i] * hist_rater_b[j]
+                              / num_scored_items)
+            d = abs(i - j) / float(num_ratings - 1)
+            numerator += d * conf_mat[i][j] / num_scored_items
+            denominator += d * expected_count / num_scored_items
+
+    return 1.0 - numerator / denominator
+
+
+def kappa(rater_a, rater_b, min_rating=None, max_rating=None):
+    """
+    Calculates the kappa
+    kappa calculates the kappa
+    value, which is a measure of inter-rater agreement between two raters
+    that provide discrete numeric ratings.  Potential values range from -1
+    (representing complete disagreement) to 1 (representing complete
+    agreement).  A kappa value of 0 is expected if all agreement is due to
+    chance.
+    kappa(rater_a, rater_b), where rater_a and rater_b
+    each correspond to a list of integer ratings.  These lists must have the
+    same length.
+    The ratings should be integers, and it is assumed that they contain
+    the complete range of possible ratings.
+    kappa(X, min_rating, max_rating), where min_rating
+    is the minimum possible rating, and max_rating is the maximum possible
+    rating
+    """
+    assert(len(rater_a) == len(rater_b))
+    if min_rating is None:
+        min_rating = min(rater_a + rater_b)
+    if max_rating is None:
+        max_rating = max(rater_a + rater_b)
+    conf_mat = confusion_matrix(rater_a, rater_b,
+                                min_rating, max_rating)
+    num_ratings = len(conf_mat)
+    num_scored_items = float(len(rater_a))
+
+    hist_rater_a = histogram(rater_a, min_rating, max_rating)
+    hist_rater_b = histogram(rater_b, min_rating, max_rating)
+
+    numerator = 0.0
+    denominator = 0.0
+
+    for i in range(num_ratings):
+        for j in range(num_ratings):
+            expected_count = (hist_rater_a[i] * hist_rater_b[j]
+                              / num_scored_items)
+            if i == j:
+                d = 0.0
+            else:
+                d = 1.0
+            numerator += d * conf_mat[i][j] / num_scored_items
+            denominator += d * expected_count / num_scored_items
+
+    return 1.0 - numerator / denominator
+
+
+def mean_quadratic_weighted_kappa(kappas, weights=None):
+    """
+    Calculates the mean of the quadratic
+    weighted kappas after applying Fisher's r-to-z transform, which is
+    approximately a variance-stabilizing transformation.  This
+    transformation is undefined if one of the kappas is 1.0, so all kappa
+    values are capped in the range (-0.999, 0.999).  The reverse
+    transformation is then applied before returning the result.
+    mean_quadratic_weighted_kappa(kappas), where kappas is a vector of
+    kappa values
+    mean_quadratic_weighted_kappa(kappas, weights), where weights is a vector
+    of weights that is the same size as kappas.  Weights are applied in the
+    z-space
+    """
+    kappas = np.array(kappas, dtype=float)
+    if weights is None:
+        weights = np.ones(np.shape(kappas))
+    else:
+        weights = weights / np.mean(weights)
+
+    # ensure that kappas are in the range [-.999, .999]
+    kappas = np.array([min(x, .999) for x in kappas])
+    kappas = np.array([max(x, -.999) for x in kappas])
+
+    z = 0.5 * np.log((1 + kappas) / (1 - kappas)) * weights
+    z = np.mean(z)
+    return (np.exp(2 * z) - 1) / (np.exp(2 * z) + 1)
+
+
+def weighted_mean_quadratic_weighted_kappa(solution, submission):
+    predicted_score = submission[submission.columns[-1]].copy()
+    predicted_score.name = "predicted_score"
+    if predicted_score.index[0] == 0:
+        predicted_score = predicted_score[:len(solution)]
+        predicted_score.index = solution.index
+    combined = solution.join(predicted_score, how="left")
+    groups = combined.groupby(by="essay_set")
+    kappas = [quadratic_weighted_kappa(group[1]["essay_score"], group[1]["predicted_score"]) for group in groups]
+    weights = [group[1]["essay_weight"].irow(0) for group in groups]
+    return mean_quadratic_weighted_kappa(kappas, weights=weights)
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ifile", help="Input file")
+    parser.add_argument("--d", help="Delimiter. Default: Comma")
+    parser.add_argument("--score", help="Score Column. Default: score")
+    parser.add_argument("--target", help="Target Column. Default: target")
+    parser.add_argument("--tag", help="Output Files Tag. Default: performance")
+    parser.add_argument("--title", help="Charts Title. Default: None")
+    
+    args = parser.parse_args()
+    infile = args.ifile
+    score = args.score if args.score else 'score'
+    target = args.target if args.target else 'target'
+    tag = args.tag if args.tag else 'performance'
+    delimiter = args.d if args.d else ','
+    title = args.title+':' if args.title else ''
+    
+    score_card=pd.read_csv(infile,delimiter=delimiter,usecols=[score,target])
+    auc,ks,ks_score=get_roc(score_card,score,target,title)
+    plt.savefig('%s_roc.png'%tag)
+    get_cum_gains(score_card,score,target,title)
+    plt.savefig('%s_cum_gains.png'%tag)
+    get_precision_recall(score_card,score,target,title)
+    plt.savefig('%s_precision_recall.png'%tag)
+    decile_analysis=get_deciles_analysis(score_card,score,target)
+    decile_analysis.to_csv('%s_decile_analysis.csv'%tag)
+    pd.Series([auc,ks,ks_score],index=['auc','ks','ks_score']).to_csv('%s_summary.csv'%tag)
+    
--- startup.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-path_to_add = [r'f:\Dropbox\Documents\Programming\Python\My_Python_Lib',
-               r'F:\Dropbox\Documents\Rutgers\Internship\BellLabs\Stuffs',
-               r'F:\Dropbox\Documents\Projects\QuantIn']
-for x in path_to_add:
-	sys.path.append(x)
